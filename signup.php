a5c790b2b532fdb2eb7924b2575622f2c413e0ed
RikuMats <rikuo62419@gmail.com>
2020-08-11 11:51:25 +0900

    メールでの認証をする部分を作った

--- signup.php
+++ signup.php
@@ -3,10 +3,16 @@ require 'phpmailer/send.php';
 $name = $_POST["name"];
 $mailAddr= $_POST["mailAddr"];
 
-//ランダム生成したい
-$verify_num = 0;
-//セッションに保存？
+//ランダム生成したい・数値としてではなく数字として6桁にしたい
 
-//send_mail($name,$mailAddr,"認証番号です",$verify_num);
+$verify_code = mt_rand(0,999999);
+//セッションに保存？
+session_start();
+$_SESSION["verify_code"] = $verify_code;
+$_SESSION["name"]=$name;
+$_SESSION["email"]=$mailAddr;
+
+//メールはテストの時だけする
+send_mail($name,$mailAddr,"認証番号です","$verify_code".PHP_EOL);
 header("location:verification.html")
 ?>
--- verification.php
+++ verification.php
@@ -1,18 +1,27 @@
 <?php
 require 'phpmailer/send.php';
 
-$user_in = $_POST["VerificationCode"];
+$user_in = $_POST["verify_code"];
 
 //セッションなどからとってくる
-$verify_num = 0;
-$name = 0;
-$mailAddr = 0;
+session_start();
 
-
+$verify_code = $_SESSION["verify_code"];
+$name = $_SESSION["name"];
+$mailAddr = $_SESSION["email"];
 //ランダム生成などする
-$pass = 0;
+echo $verify_code;
+if ($user_in !=$verify_code){
+    echo "コードが違っています<br> <a href='verification.html'>戻る</a>";
+}else{
+    $pass = substr(bin2hex(random_bytes(6)),0,6);
 
-send_mail($name,$mailAddr,"sign up complete","お疲れ様です".PHP_EOL."仮パスワード :".$pass);
+    //データベースに名前　パスワード　メールアドレスを保存
+
+
+    send_mail($name,$mailAddr,"sign up complete","お疲れ様です".PHP_EOL."仮パスワード :".$pass);
+}
+
 
 
 ?>
