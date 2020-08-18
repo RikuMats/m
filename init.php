09d6f71159cc67d45d28ce3888ca84119668d4b3
RikuMats <rikuo62419@gmail.com>
2020-08-18 18:36:57 +0900

    bookmasterとuser_book_tableの登録の部分をつくった

--- init.php
+++ init.php
@@ -24,10 +24,18 @@ $sql = "CREATE TABLE IF NOT EXISTS bookmaster"
     ."id INT AUTO_INCREMENT PRIMARY KEY,"
     ."title char(32),"
     ."author char(32),"
-    ."pablisher char(32)"
+    ."publisher char(32)"
     .")";
 
 $stmt = $pdo->query($sql);
 
+//ユーザと本と未読、読了などの情報を保持する
+$sql = "CREATE TABLE IF NOT EXISTS user_book_table"
+    ."("
+    ."book_id INT,"
+    ."user_id INT,"
+    ."status char(10)"//unread,red
+    .")";
 echo "end";
+$stmt = $pdo->query($sql);
 ?>
--- register_new_book.php
+++ register_new_book.php
@@ -1,10 +1,40 @@
 <?php
+require 'config_sql.php';
+session_start();
+
 $title = htmlspecialchars($_POST["title"]);
 $author = htmlspecialchars($_POST["author"]);
 $publisher = htmlspecialchars($_POST["publisher"]);
+$file = $_FILES["img"];
 
-//マスタにデータがある確認
-//なければ登録
+$pdo = new PDO(DSN,USER,PASS,array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_WARNING));
+//まずはマスタを確認なければインサート
+//もしくはとりあえずインサートを実行し、セレクトでbook_idを取得
+$sql = "INSERT IGNORE INTO bookmaster (title,author,publisher) VALUES (:title,:author,:publisher)";
+$prestmt = $pdo->prepare($sql);
+$prestmt->bindParam(':title',$title,PDO::PARAM_STR);
+$prestmt->bindParam(':author',$author,PDO::PARAM_STR);
+$prestmt->bindParam(':publisher',$publisher,PDO::PARAM_STR);
+$prestmt->execute();
+
+$sql = "SELECT id FROM bookmaster WHERE title=:title AND author=:author AND publisher=:publisher";
+$prestmt = $pdo->prepare($sql);
+$prestmt->bindParam(':title',$title,PDO::PARAM_STR);
+$prestmt->bindParam(':author',$author,PDO::PARAM_STR);
+$prestmt->bindParam(':publisher',$publisher,PDO::PARAM_STR);
+$prestmt->execute();
+$row = $prestmt->fetch();
+$book_id = $row['id'];//マスタから持ってきた本のid
+
+$sql = "INSERT IGNORE INTO users_book_table (book_id,user_id,status) VALUES(:book_id,:user_id,:status)";
+$prestmt = $pdo->prepare($sql);
+$prestmt->bindParam(':book_id',$book_id,PDO::PARAM_INT);
+$prestmt->bindParam(':user_id',$_SESSION['id'],PDO::PARAM_INT);
+$prestmt->bindParam(':status',"unread",PDO::PARAM_STR);
+$prestmt->execute();
+
+$filepath ="img/".$book_id;
+move_uploaded_file($file['tmp_name'],$filepath);
 
 //個人と本を結びつけるデータベースに登録
 //未読、読了などの状態を保持できるようにする
